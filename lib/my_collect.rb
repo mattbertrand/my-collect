b8b4b3dc5d7b973f61efbfea444ba06136c648f9
github username <github email address>
2020-11-15 19:35:53 -0800

    commit tree 3a64df8b975d85e6e5606e9db49d905d4f5804ac

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+    counter = 0
+    collect = []
+    
+    while counter < array.length
+    collect << yield(array[counter])
+        counter += 1
+    end
+    collect
+end
